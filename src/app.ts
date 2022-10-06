91f43be071a1ce9bf859dcd84c168ff097c79049
costalucs <lucasdeslz@hotmail.com>
2022-10-06 16:57:06 -0300

    [feat]: route get products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import ProductController from './controller/ProductController';
+// import ProductController from './controller/ProductController';
 
 const app = express();
+const productController = new ProductController();
 
 app.use(express.json());
 
+app.get('/products', productController.getAll);
 export default app;
