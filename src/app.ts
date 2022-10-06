a151d2ca9a7dfc00b1eefa9f1391ed0ac95da93f
costalucs <lucasdeslz@hotmail.com>
2022-10-06 17:22:37 -0300

    [feat]: order get route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import OrderController from './controller/OrderController';
 import ProductController from './controller/ProductController';
-// import ProductController from './controller/ProductController';
 
 const app = express();
 const productController = new ProductController();
+const orderController = new OrderController();
 
 app.use(express.json());
 
 app.get('/products', productController.getAll);
+app.get('/orders', orderController.getAll);
+
 export default app;
