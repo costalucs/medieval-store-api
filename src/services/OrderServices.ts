dd10b39b77d064f114ab2a1e90849204e3e44561
costalucs <lucasdeslz@hotmail.com>
2022-10-06 17:23:12 -0300

    [feat]: OrderServices

---
+++ src/services/OrderServices.ts
@@ -0,0 +1,16 @@
+import { Order } from '../interfaces';
+import connection from '../models/connection';
+import OrderModel from '../models/OrderModel';
+
+export default class OrderServices {
+  model: OrderModel;
+
+  constructor() {
+    this.model = new OrderModel(connection);
+  }
+
+  async getAll(): Promise<Order[]> {
+    const orders = await this.model.getAll();
+    return orders;
+  }
+}
