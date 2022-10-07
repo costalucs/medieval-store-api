a4ed14f6f1ef3ec4c5f49e816338c2cb6fc5b177
costalucs <lucasdeslz@hotmail.com>
2022-10-07 11:40:27 -0300

    [feat]: ProductControllet insert and FindByID

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -4,8 +4,20 @@ import ProductServices from '../services/ProductServices';
 export default class ProductController {
   constructor(private productService = new ProductServices()) {}
 
-  public getAll = async (_req:Request, response: Response) => {
+  getAll = async (_req:Request, res: Response) => {
     const products = await this.productService.getAll();
-    response.status(200).json(products);
+    res.status(200).json(products);
+  };
+
+  insertProduct = async (req: Request, res: Response) => {
+    const product = req.body;
+    const result = await this.productService.insertProduct(product);
+    res.status(201).json(result);
+  };
+
+  getOne = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await this.productService.findOne(+id);
+    res.status(201).json(result);
   };
 }
