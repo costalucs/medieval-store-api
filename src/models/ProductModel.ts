fe179b704edaa89ce5e5500cc8e7c8b56138a07c
costalucs <lucasdeslz@hotmail.com>
2022-10-07 11:40:05 -0300

    [feat]: ProductModel insert

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import { Product } from '../interfaces';
 
 export default class ProcuctModel {
@@ -13,5 +13,18 @@ export default class ProcuctModel {
       .execute('SELECT * FROM Trybesmith.Products;');
     const [rows] = result;
     return rows as Product[];
+  }
+
+  async findOne(id: number): Promise<Product[] | []> {
+    const [result] = await this.connection
+      .query('SELECT * FROM Trybesmith.Products WHERE id = ?', [id]);
+    return result as Product[];
+  }
+
+  async insertProduct({ name, amount }: Product): Promise<Product> {
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+    const [{ insertId }] = await this.connection
+      .execute<ResultSetHeader>(query, [name, amount]);
+    return { id: insertId, name, amount };
   }
 }
