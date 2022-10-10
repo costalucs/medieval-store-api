65cc8f38d797131faea2a557902313ddd1031b25
costalucs <lucasdeslz@hotmail.com>
2022-10-10 15:22:58 -0300

    [feat]: route + midleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import OrderController from './controller/OrderController';
 import ProductController from './controller/ProductController';
 import UserController from './controller/UserController';
 import LoginMiddleware from './middlewares/LoginMiddleware';
+import validateProductMiddleware from './middlewares/validateProductMiddleware';
+import ValidateUserMiddleware from './middlewares/ValidateUserMiddleware';
 
 const app = express();
 const productController = new ProductController();
@@ -16,8 +18,8 @@ app.get('/products', productController.getAll);
 app.get('/products/:id', productController.getOne);
 app.get('/orders', orderController.getAll);
 
-app.post('/products', productController.insertProduct);
-app.post('/users', userController.insertUser);
+app.post('/products', validateProductMiddleware, productController.insertProduct);
+app.post('/users', ValidateUserMiddleware, userController.insertUser);
 app.post('/login', LoginMiddleware, loginController.login);
 
 export default app;
